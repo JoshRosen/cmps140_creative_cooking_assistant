3398fe2ae5b3e73aa0ab664452c87469f170f5ed
Mike Wilson <mijowils@ucsc.edu>
2011-02-05 15:25:26 -0800

    Updated NLU to return multiple messages.

--- chatbot.py
+++ chatbot.py
@@ -6,6 +6,7 @@ from nlg import NaturalLanguageGenerator
 from nlu import NaturalLanguageUnderstander
 from dm import DialogueManager
 from data_structures import ConversationState
+from data_structures import ParsedInputMessage
 
 # Monkey-patch the Python 2.7 logger.getChild() method into the logger class,
 # to maintain backwards-compatibility with Python 2.6.
@@ -36,7 +37,7 @@ class Chatbot(object):
         self.database = database
         self.log = logger
         self.nlg = NaturalLanguageGenerator(logger.getChild('nlg'))
-        self.nlu = NaturalLanguageUnderstander(logger.getChild('nlu'))
+        self.nlu = NaturalLanguageUnderstander(0.0, logger.getChild('nlu'))
         self.dm = DialogueManager(database, logger.getChild('dm'))
         self.log.debug("Chatbot instantiated")
 
@@ -50,7 +51,7 @@ class Chatbot(object):
         conversation_state.last_user_input = user_input
         parsed_input = self.nlu.parse_input(user_input, conversation_state)
         self.log.debug('%12s = "%s"' % ('parsed_input', parsed_input))
-        content_plan = self.dm.plan_response(parsed_input, conversation_state)
+        content_plan = self.dm.plan_response(parsed_input[0], conversation_state)
         self.log.debug('%12s = "%s"' % ('content_plan', content_plan))
         bot_response = self.nlg.generate_response(content_plan,
             conversation_state)
@@ -63,4 +64,5 @@ class Chatbot(object):
         conversation state.
         """
         conversation_state = ConversationState()
+        self.nlu.register_message(ParsedInputMessage, conversation_state)
         return ("Hello!", conversation_state)
