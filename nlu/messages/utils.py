5582b55229a2b54e29d9cc428a797b130f1ba2bb
Mike Wilson <mijowils@ucsc.edu>
2011-03-11 19:32:15 -0500

    Forgot to add new utils file.

---
+++ nlu/messages/utils.py
@@ -0,0 +1,56 @@
+import nltk
+from nltk.corpus import wordnet
+
+def extract_close_keywords(keywords, tokenized_string, minDistance):
+    """
+    >>> raw_input_string = 'I like fish.'
+    >>> tokenizer = nltk.WordPunctTokenizer()
+    >>> tokenized_string = tokenizer.tokenize(raw_input_string)
+    >>> keywords = ['like.v.05']
+    >>> extract_close_keywords(keywords, tokenized_string, 2)
+    ['like']
+    """
+    closeWords = []
+    for keyword in keywords:
+        keywordSyn = wordnet.synset(keyword)
+        for word in tokenized_string:
+            for wordSyn in wordnet.synsets(word):
+                distance = keywordSyn.shortest_path_distance(wordSyn)
+                if distance != None and distance <= minDistance:
+                    closeWords.append(word)
+                    break
+    return closeWords
+    
+def extract_words_from_list(word_list, string_list, enum=False):
+    """
+    Returns (index, word) or a list of words for words which occur in both
+    lists.
+    """
+    
+    for i, word in enumerate(string_list):
+        if word in word_list:
+            if enum:
+                yield (i, word)
+            else:
+                yield word
+                
+def get_keyword_confidence(raw_input_string, keywords, minDistance):
+    bestDistance = float('inf')
+
+    tokenizer = nltk.WordPunctTokenizer()
+    tokenized_string = tokenizer.tokenize(raw_input_string)
+
+    # Find the best keyword synset distance to input string
+    for keyword in keywords:
+        keywordSyn = wordnet.synset(keyword)
+        for word in tokenized_string:
+            for wordSyn in wordnet.synsets(word):
+                distance = keywordSyn.shortest_path_distance(wordSyn)
+                if distance != None:
+                    bestDistance = min(bestDistance, distance)
+                
+    if bestDistance <= minDistance:
+        return (1-(float(bestDistance)/minDistance)) * .5 + .5
+    else:
+        return 0.0
+
