e80b17f324e849828f92be56f4aee8d0d0b2208d
Josh Rosen <jodarose@ucsc.edu>
2011-03-16 00:38:34 -0400

    Modify test_nlu.py to pretty-print ParsedInputMessages.

--- test_nlu.py
+++ test_nlu.py
@@ -1,58 +1,37 @@
-import cPickle
-import code
 import logging
 import re
-from nlg import NaturalLanguageGenerator
+import pprint
 from nlu import NaturalLanguageUnderstander
-from nlu.messages import *
-# from dm import DialogueManager
-from nlu.messages.parsed_input_message import ParsedInputMessage
-from database import Database
-
-dburl='sqlite:///test_database.sqlite'
-db = Database(dburl)
-
-logger = logging.getLogger()
-
-nlg = NaturalLanguageGenerator(logger)
-nlu = NaturalLanguageUnderstander(0.5, logger)
-# dm = DialogueManager(db, logger.getChild('dm'))
-
-confidenceThreshold = .5
-nlu = NaturalLanguageUnderstander(confidenceThreshold, logger)
-
-# Register the NLU messages we want
-nlu.register_message(YesNoMessage)
-nlu.register_message(SearchMessage)
-nlu.register_message(SystemMessage)
+from nlu.messages import YesNoMessage, SearchMessage, SystemMessage
+from itertools import islice
 
 
-fil = open('INGREDIENTS.txt', 'r')
+LIMIT = 4 # How many messages to test
 
-lno = 0;
-cnt = 0;
-msgs = [] 
-lines = [] 
-input_txt = fil.readline(); lno += 1;
-while input_txt:
-    if re.search('^\s*($|#)', input_txt): # comment or empty lines.
-        input_txt = fil.readline(); lno += 1;
-        continue    # ignore empty lines
-    # print('>>>>>>>>>>>>>>>>>>>>>>>>>')
-    msg = nlu.parse_input(input_txt)
-    lines.append(input_txt)
-    msgs.append(msg)
-    cnt += 1
-    # print('\nL:%s' % input_txt)
-    # print(msg)
-    # print('<<<<<<<<<<<<<<<<<<<<<<<<<')
-    input_txt = fil.readline(); lno += 1;
-    if cnt > 4:
-        break
 
-def show_input(lines, msgs):
-    for inx in range(len(lines)):
-        print(lines[inx])
-        print(msgs[inx])
+def main():
+    logger = logging.getLogger()
 
-show_input(lines, msgs)
+    confidence_threshold = .5
+    nlu = NaturalLanguageUnderstander(confidence_threshold, logger)
+
+    # Register the NLU messages we want
+    nlu.register_message(YesNoMessage)
+    nlu.register_message(SearchMessage)
+    nlu.register_message(SystemMessage)
+
+    fil = open('INGREDIENTS.txt', 'r')
+
+    # Skip comments and empty lines
+    input_messages = (l.strip() for l in fil if not re.search('^\s*($|#)', l))
+    for line in islice(input_messages, 0, LIMIT):
+        messages = nlu.parse_input(line)
+        print line
+        for message in messages:
+            print message.__class__
+            print pprint.pformat(message.frame)
+        print
+
+
+if __name__ == '__main__':
+    main()
